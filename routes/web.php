8e3a05757c3f96d9665777458c0cb1524f3d7db7
joruno <hvereliem@student.ciputra.ac.id>
2026-08-13 22:35:04 +0700

    feat: implement category admin CRUD and auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
-use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\CategoryController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
     Route::resource('products', ProductController::class);
     Route::resource('categories', CategoryController::class);
 });
 
+Route::get('/dashboard', function () {
+    return redirect()->route('admin.categories.index');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 require __DIR__.'/auth.php';
